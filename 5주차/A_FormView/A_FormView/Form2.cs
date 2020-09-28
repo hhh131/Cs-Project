a9ea5748dd148e8c49eafd4b810f20d3b7421338
hhh131 <gmlrnjs131@naver.com>
2020-09-29 03:11:15 +0900

    5주차 수업

---
+++ 5주차/A_FormView/A_FormView/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_FormView
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private double o = 0.0;
+
+        public string SetText
+        {
+            set { this.Text = value; }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(o<100.0)
+            {
+                o = o + 3.6;
+                float c = Convert.ToSingle(o);
+                float f = c / 100;
+                this.Opacity = f;
+
+            }
+            else
+            {
+                this.Opacity = Convert.ToSingle(100 / 100);
+                o = 0.0;
+                this.Timer.Enabled = false;
+            }
+        }
+    }
+}
